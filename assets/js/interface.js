164e09969fb7f57c7d62546b3368f2307bb07677
Ashik <lovelyhansu789@gmail.com>
2021-04-14 11:38:43 +0530

    first commit

---
+++ assets/js/interface.js
@@ -0,0 +1,55 @@
+﻿( function($) {
+  'use strict';
+$(function(e) {
+
+/*-------------------------------------------------------------------------------
+	  Smooth scroll to anchor
+	-------------------------------------------------------------------------------*/
+	var navbar=$('.js-navbar');
+	var navbarAffixHeight=80
+	$('.js-target-scroll').on('click', function(e) {
+		var target = $(this.hash);
+		if (target.length) {
+			$('html,body').animate({
+				scrollTop: (target.offset().top - navbarAffixHeight + 1)
+			}, 1000);
+			return false;
+		}
+	});
+	$('body').scrollspy({
+			offset:  navbarAffixHeight + 1
+		});
+	$('.navbar-nav a.js-target-scroll ').on('click',function(){
+	  if($('#navigation').removeClass('in')) {
+	  }
+	  });	
+
+/*-------------------------------------------------------------------------------
+	  Sticky-Header
+	-------------------------------------------------------------------------------*/
+$(window).scroll(function(){
+  var sticky = $('#header'),
+      scroll = $(window).scrollTop();
+
+  if (scroll >= 100) sticky.addClass('sticky');
+  else sticky.removeClass('sticky');
+});
+    
+     $(".customer-say .owl-carousel").owlCarousel({
+        loop: true,
+        margin: 35,
+        nav: true,
+        center: true,
+        autoplay: true,
+        navText: [
+            '<i class="fa fa-angle-left"></i>',
+            '<i class="fa fa-angle-right"></i>'
+        ],
+        items: 1
+    });
+	  	
+	
+	});
+
+
+})(jQuery);
